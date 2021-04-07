68dd1e32daaa6bb0ae7767e3aadbc93541eba4a4
lyana <lyana-m@yandex.ru>
2021-04-07 18:23:10 +0500

    feat: solve 4th task

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,49 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const newMatrix = [];
+  for (let i = 0; i < matrix.length; i++) {
+    const arr = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      let counter = 0;
+      // down
+      if (i !== (matrix.length - 1) && matrix[i + 1][j]) {
+        counter++;
+      }
+      // down left
+      if (i !== (matrix.length - 1) && j !== 0 && matrix[i + 1][j - 1]) {
+        counter++;
+      }
+      // down right
+      if (i !== (matrix.length - 1) && j !== matrix[i].length - 1 && matrix[i + 1][j + 1]) {
+        counter++;
+      }
+      // left
+      if (j !== 0 && matrix[i][j - 1]) {
+        counter++;
+      }
+      // right
+      if (j !== (matrix[i].length - 1) && matrix[i][j + 1]) {
+        counter++;
+      }
+      // top
+      if (i !== 0 && matrix[i - 1][j]) {
+        counter++;
+      }
+      // top left
+      if (i !== 0 && j !== 0 && matrix[i - 1][j - 1]) {
+        counter++;
+      }
+      // top right
+      if (i !== 0 && j !== (matrix[i].length - 1) && matrix[i - 1][j + 1]) {
+        counter++;
+      }
+      arr.push(counter);
+    }
+    newMatrix.push(arr);
+  }
+  return newMatrix;
 }
 
 module.exports = minesweeper;
