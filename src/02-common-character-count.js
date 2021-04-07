606cdb3e7ca097c7bd810898e8b888eb12ba2030
lyana <lyana-m@yandex.ru>
2021-04-07 14:22:05 +0500

    feat: solve 3rd task

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -10,8 +10,8 @@
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2) {
-  const arr1 = s1.split('').sort();
-  const arr2 = s2.split('').sort();
+  const arr1 = s1.split('');
+  const arr2 = s2.split('');
   const count1 = {};
   const count2 = {};
   let sum = 0;
--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,18 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const sortedArr = arr.filter((item) => item !== -1).sort((a, b) => a - b);
+  const resultedArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      resultedArr.push(-1);
+    } else {
+      resultedArr.push(sortedArr[0]);
+      sortedArr.splice(0, 1);
+    }
+  }
+  return resultedArr;
 }
 
 module.exports = sortByHeight;
