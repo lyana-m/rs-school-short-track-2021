aeaf05de10bcb225ee007b10c212901d62de5303
lyana <lyana-m@yandex.ru>
2021-04-08 22:53:11 +0500

    feat: solve 11th task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,19 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  const newArr = [];
+  let counter = 1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[i + 1]) {
+      newArr.push(counter === 1 ? `${arr[i]}` : `${counter}${arr[i]}`);
+      counter = 1;
+    } else {
+      counter++;
+    }
+  }
+  return newArr.join('');
 }
 
 module.exports = encodeLine;
